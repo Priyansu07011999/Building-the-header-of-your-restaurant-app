dfc36d628685fb700e0648c0dd22946883966cd5
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-14 18:11:42 +0530

    Update Header.jsx

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -3,18 +3,19 @@ import veg from '../../assets/meals.jpg';
 import classes from './Header.module.css'; // Importing CSS module
 import HeaderCardButton from './HeaderCardButton';
 
-function Header(props) {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
-        <h1 className='text-3xl'>ReactMeals</h1>
-        <HeaderCardButton/>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton />
       </header>
-      <div className={classes['main-img']}>
-        <img className='w-full' src={veg} alt="Image"/>
+      <div className={classes['main-image']}>
+        <img src={veg} alt='A table full of delicious food!' />
       </div>
     </>
   );
-}
+};
+
 
 export default Header;
